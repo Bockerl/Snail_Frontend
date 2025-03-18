7b1ec5dcda1477b0481840a4c1192b8386f377c3
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2025-03-18 19:25:16 +0900

    ✨ feat: 앱 헤더 컴포넌트 기능 추가

---
+++ src/components/common/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="header">
+      <img src="@/assets/icons/common/SnailHeaderlogo.svg" alt="Snail Logo" class="logo" />
+    </header>
+  </template>
+  
+  <style scoped>
+  .header {
+    height: 56px;
+    width: 100%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0px 0px 8px 3px rgba(86, 97, 246, 0.3);
+    position: sticky;
+    top: 0;
+    z-index: 10;
+  }
+  
+  .logo {
+    height: 18px;
+  }
+  </style>
+  
